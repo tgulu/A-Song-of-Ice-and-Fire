38d7f3bd1f4279a630fdce1d2a6b9f5aa14fb739
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-09-03 16:57:23 +0100

    valuedate information about chars

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 async function fetchData() {
   try {
+    const id = document.getElementById("characterName").value.toLowerCase();
+    const response = await fetch(`https://thronesapi.com/api/v2/Characters`);
+
+    if (!response.ok) {
+      throw new Error("Could not fetch resource");
+    }
+    const data = await response.json();
   } catch (error) {
     console.error(error);
     alert("Error fetching data. Please try again.");
